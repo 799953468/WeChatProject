ede3acea257110ddb7a790e72a96f1750cd037de
799953468 <799953468@qq.com>
2020-10-19 16:20:54 +0800

    Update

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ Page({
     page3: true,
     page4: true,
     page5: true,
+    page6: true,
+    page7: true,
+    page8: true,
+    page9: true,
+    page10: true
   },
 
   onLoad: function (options) {
@@ -110,100 +115,227 @@ Page({
     this.setData({
       show2_6: 'bounce animated show'
     })
-    setTimeout(this.showarrow,1000)
+    setTimeout(this.showarrow, 1000)
   },
-  show3:function(){
+  show3: function () {
     this.setData({
       showbg: 'zoomIn animated show',
       show1: 'fadeInLeft animated show'
     })
     setTimeout(this.show3_1, 1000)
   },
-  show3_1: function(){
+  show3_1: function () {
     this.setData({
       show3_2: 'flipInY animated show'
     })
-    setTimeout(this.show3_2,1000)
+    setTimeout(this.show3_2, 1000)
   },
-  show3_2: function(){
+  show3_2: function () {
     this.setData({
       show3_3: 'bounce animated show'
     })
-    setTimeout(this.show3_3,1000)
+    setTimeout(this.show3_3, 1000)
   },
-  show3_3: function(){
+  show3_3: function () {
     this.setData({
       show3_4: 'flipInX animated show'
     })
-    setTimeout(this.show3_4,1000)
+    setTimeout(this.show3_4, 1000)
   },
-  show3_4: function(){
+  show3_4: function () {
     this.setData({
       show3_5: 'fadeInLeft animated show'
     })
-    setTimeout(this.show3_5,1000)
+    setTimeout(this.show3_5, 1000)
   },
-  show3_5: function(){
+  show3_5: function () {
     this.setData({
       show3_6: 'fadeInRight animated show'
     })
-    setTimeout(this.showarrow,1000)
+    setTimeout(this.showarrow, 1000)
   },
-  show4: function(){
+  show4: function () {
     this.setData({
       showbg: 'zoomIn animated show',
       show1: 'fadeInLeft animated show'
     })
     setTimeout(this.show4_1, 1000)
   },
-  show4_1: function(){
+  show4_1: function () {
     this.setData({
       show4_2: 'flipInY animated show'
     })
-    setTimeout(this.show4_2,1000)
+    setTimeout(this.show4_2, 1000)
   },
-  show4_2: function(){
+  show4_2: function () {
     this.setData({
       show4_3: 'bounce animated show'
     })
-    setTimeout(this.show4_3,1000)
+    setTimeout(this.show4_3, 1000)
   },
-  show4_3: function(){
+  show4_3: function () {
     this.setData({
       show4_4: 'flipInX animated show'
     })
-    setTimeout(this.show4_4,1000)
+    setTimeout(this.show4_4, 1000)
   },
-  show4_4: function(){
+  show4_4: function () {
     this.setData({
       show4_5: 'fadeInRight animated show'
     })
-    setTimeout(this.show4_5,1000)
+    setTimeout(this.show4_5, 1000)
   },
-  show4_5: function(){
+  show4_5: function () {
     this.setData({
       show4_6: 'fadeInLeft animated show'
     })
-    setTimeout(this.showarrow,1000)
+    setTimeout(this.showarrow, 1000)
   },
-  show5: function(){
+  show5: function () {
     this.setData({
       showbg: 'zoomIn animated show',
       show1: 'fadeInLeft animated show'
     })
     setTimeout(this.show5_1, 1000)
   },
-  show5_1: function(){
+  show5_1: function () {
     this.setData({
       show5_2: 'fadeInDown animated show'
     })
-    setTimeout(this.show5_2,1000)
+    setTimeout(this.show5_2, 1000)
   },
-  show5_2: function(){
+  show5_2: function () {
     this.setData({
       show5_3: 'bounce animated show'
     })
-    setTimeout(this.showarrow,1000)
+    setTimeout(this.showarrow, 1000)
+  },
+  show6: function () {
+    this.setData({
+      showbg: 'zoomIn animated show',
+      show1: 'fadeInLeft animated show'
+    })
+    setTimeout(this.show6_1, 1000)
+  },
+  show6_1: function () {
+    this.setData({
+      showbg: 'zoomIn animated show',
+      show6_2: 'fadeInDown animated show'
+    })
+    setTimeout(this.show6_2, 1000)
+  },
+  show6_2: function () {
+    this.setData({
+      show6_4: 'fadeInUp animated show'
+    })
+    setTimeout(this.show6_3, 1000)
+  },
+  show6_2: function () {
+    this.setData({
+      show6_3: 'swing animated show'
+    })
+    setTimeout(this.showarrow, 1000)
+  },
+  show7: function () {
+    this.setData({
+      showbg: 'zoomIn animated show',
+      show1: 'fadeInLeft animated show'
+    })
+    setTimeout(this.show7_1, 1000)
+  },
+  show7_1: function () {
+    this.setData({
+      show7_2: 'slideInDown animated show'
+    })
+    setTimeout(this.show7_2, 1000)
+  },
+  show7_2: function () {
+    this.setData({
+      show7_3: 'fadeInUp animated show'
+    })
+    setTimeout(this.showarrow, 1000)
+  },
+  show8: function () {
+    this.setData({
+      showbg: 'zoomIn animated show',
+      show1: 'fadeInLeft animated show'
+    })
+    setTimeout(this.show8_1, 1000)
+  },
+  show8_1: function () {
+    this.setData({
+      show8_2: 'slideInDown animated show'
+    })
+    setTimeout(this.show8_2, 2000)
+  },
+  show8_2: function () {
+    this.setData({
+      show8_3: 'slideInLeft animated show',
+      show8_5: 'slideInRight animated show'
+    })
+    setTimeout(this.show8_3, 1000)
+  },
+  show8_3: function () {
+    this.setData({
+      show8_4: 'zoomIn animated show'
+    })
+    setTimeout(this.showarrow, 1000)
+  },
+  show9: function () {
+    this.setData({
+      showbg: 'zoomIn animated show',
+      show1: 'fadeInLeft animated show'
+    })
+    setTimeout(this.show9_1, 1000)
+  },
+  show9_1: function () {
+    this.setData({
+      show9_2: 'rubberBand animated show'
+    })
+    setTimeout(this.show9_2, 1000)
+  },
+  show9_2: function () {
+    this.setData({
+      show9_4: 'lightSpeedIn animated show'
+    })
+    setTimeout(this.show9_3, 1000)
+  },
+  show9_3: function () {
+    this.setData({
+      show9_3: 'swing animated show'
+    })
+    setTimeout(this.showarrow, 1000)
+  },
+  show10: function () {
+    this.setData({
+      showbg: 'zoomIn animated show',
+      show1: 'fadeInLeft animated show'
+    })
+    setTimeout(this.show10_1, 1000)
+  },
+  show10_1: function () {
+    this.setData({
+      show10_2: 'fadeInDown animated show'
+    })
+    setTimeout(this.show10_2, 1000)
+  },
+  show10_2: function () {
+    this.setData({
+      show10_3: 'fadeInRight animated show',
+      show10_4: 'fadeInLeft animated show'
+    })
+    setTimeout(this.show10_3, 1000)
+  },
+  show10_3: function () {
+    this.setData({
+      show10_5: 'flipInY animated show'
+    })
+    setTimeout(this.show10_4, 1000)
+  },
+  show10_4: function () {
+    this.setData({
+      show10_6: 'bounce animated show'
+    })
   },
   onReady: function () {
     this.showbg()
@@ -325,18 +457,66 @@ Page({
             })
             setTimeout(this.show5, 100);
             break;
-            case 5:
-              this.setData({
-                page6: false,
-                page5: true,
-                flag: false,
-                showbg: '',
-                show1: '',
-                showarrow: '',
-                index: 6
-              })
-              setTimeout(this.show6, 100);
-              break;
+          case 5:
+            this.setData({
+              page6: false,
+              page5: true,
+              flag: false,
+              showbg: '',
+              show1: '',
+              showarrow: '',
+              index: 6
+            })
+            setTimeout(this.show6, 100);
+            break;
+          case 6:
+            this.setData({
+              page7: false,
+              page6: true,
+              flag: false,
+              showbg: '',
+              show1: '',
+              showarrow: '',
+              index: 7
+            })
+            setTimeout(this.show7, 100);
+            break;
+          case 7:
+            this.setData({
+              page8: false,
+              page7: true,
+              flag: false,
+              showbg: '',
+              show1: '',
+              showarrow: '',
+              index: 8
+            })
+            setTimeout(this.show8, 100);
+            break;
+          case 8:
+            this.setData({
+              page9: false,
+              page8: true,
+              flag: false,
+              showbg: '',
+              show1: '',
+              showarrow: '',
+              index: 9
+            })
+            setTimeout(this.show9, 100);
+            break;
+          case 9:
+            this.setData({
+              page10: false,
+              page9: true,
+              flag: false,
+              showbg: '',
+              show1: '',
+              showarrow: '',
+              index: 10
+            })
+            setTimeout(this.show10, 100);
+            break;
         }
       }
     }
